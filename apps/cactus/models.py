c3857b015c3680cdae5cbf9b405dc651dde40b29
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-06-10 14:17:55 -0400

    captus_shopi create models cactus, category and install package Pillow

--- apps/cactus/models.py
+++ apps/cactus/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    
+    categoy_name = models.CharField('Nombre de la categoria', max_length=255, blank=False , null=False)
+    # TODO: Define fields here
+
+    class Meta:
+        """Meta definition for Category."""
+
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categorys'
+
+    def __str__(self):
+        """Unicode representation of Category."""
+        return self.categoy_name
+
+
+class Plant(models.Model):
+    """Model definition for Pnatas."""
+    name = models.CharField('Nombre de la planta', max_length=255, blank=False , null=False)
+    description = models.TextField('Descripcion', blank= False, null=False)
+    cost = models.DecimalField('Costo', max_digits=10,  decimal_places=2, blank= False, null= False)
+    image = models.ImageField('Imagen del producto', upload_to='products_image/', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Category',blank=False, null= False)
+    # TODO: Define fields here
+
+    class Meta:
+        """Meta definition for MODELNAME."""
+
+        verbose_name = 'MODELNAME'
+        verbose_name_plural = 'MODELNAMEs'
+
+    def __str__(self):
+        return self.name
