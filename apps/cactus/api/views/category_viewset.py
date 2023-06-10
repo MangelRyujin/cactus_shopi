8c2de61426658c2f9f882b025d6cc969b054e665
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-06-10 15:39:20 -0400

    cactus_shopi create viewset method create of model Category

--- apps/cactus/api/views/category_viewset.py
+++ apps/cactus/api/views/category_viewset.py
@@ -18,6 +18,13 @@ class CategoryViewSet(viewsets.GenericViewSet):
         categorys = self.get_queryset()
         if categorys.exists():
             categorys_serializers = self.serializer_class(categorys,many = True)
-            return Response({'categorys':categorys_serializers.data}, status= status.HTTP_200_OK)
-        
+            return Response(categorys_serializers.data, status= status.HTTP_200_OK)
         return Response({'message':'No existen categorias'}, status= status.HTTP_200_OK)
+    
+    def create(self,request,*args, **kargs):
+        category_serializers = self.serializer_class(data = request.data)
+        if category_serializers.is_valid():
+            category_serializers.save()
+            return Response({'message':'Categoria creada correctamente'}, status=status.HTTP_201_CREATED)
+        return Response({'errors':category_serializers.errors}, status= status.HTTP_400_BAD_REQUEST)
+    
