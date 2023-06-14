130245d53142eb649ae45637664ba7fbea63a479
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-06-14 11:55:28 -0400

    cactus shopi add MEDIA_URL to urls

--- captus_shopi/settings/production.py
+++ captus_shopi/settings/production.py
@@ -18,6 +18,8 @@ DATABASES = {
 
 STATIC_URL = 'static/'
 
+
+
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- captus_shopi/urls.py
+++ captus_shopi/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 from django.urls import path,include,re_path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi 
-
+from django.conf.urls.static import static
 from rest_framework import permissions
-
+from django.conf import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -42,3 +42,6 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger',cache_timeout=0), name = 'schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',cache_timeout=0), name = 'schema-swagger-redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
