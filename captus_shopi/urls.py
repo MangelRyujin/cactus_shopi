0af3227b4137043fbce7d1aacea58a9d6618ce29
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-06-14 13:28:31 -0400

    cactus shopi .gitignore add media

--- captus_shopi/urls.py
+++ captus_shopi/urls.py
@@ -52,9 +52,3 @@ urlpatterns+=[
         'document_root': settings.MEDIA_ROOT,
     })
 ]
-
-
-
-
-# if settings.DEBUG:
-#     urlpatterns += static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
