6dd15e07bb6ea8b4b126bdd680d3cc90ee813ba8
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-06-20 14:45:04 -0400

    shopi_cactus create model User

--- captus_shopi/settings/base.py
+++ captus_shopi/settings/base.py
@@ -39,6 +39,7 @@ BASE_APPS = [
 LOCAL_APPS = [
     'apps.cactus',
     'apps.pedido',
+    'apps.users',
 ]
 
 
@@ -116,7 +117,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+AUTH_USER_MODEL = 'users.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
